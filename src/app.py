c04ff0ec7ea8ef6acda1a4892670bc4aa1bfa8e2
Zenahr Barzani <zenahr.barzani@jpsc.intern>
2020-10-17 20:13:11 +0200

    add plot option

--- src/app.py
+++ src/app.py
@@ -5,22 +5,32 @@ import os
 import nltk.corpus
 import texts
 from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
 
 def tokenize(text):
     return word_tokenize(text)
 
+def frequency_distinct(tokens, plot=False):
+
+    frequency_distinct = FreqDist(tokens)
+    if (plot):
+       frequency_distinct.plot()
+    return frequency_distinct
 
 
 def log():
     os.system('cls')
     tokens = tokenize(texts.one)
-    log_section('TOKENS')
-    print(tokens)
+    freq_dist = frequency_distinct(tokens)
 
-def log_section(title):
+    log_section('TOKENS', tokens)
+    log_section('FREQUENCY DISTINCT', freq_dist)
+
+def log_section(title, data):
     print(title)
     print("-"*200)
     print()
+    print(data)
 
 
 log()
